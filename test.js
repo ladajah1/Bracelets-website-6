28fe8fdd5d444662411e30c224ae2cd98e23d9c6
ladajah1 <98850547+ladajah1@users.noreply.github.com>
2023-06-02 18:07:19 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,19 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 // A function that changes text in an element with id 'demo'\
 function changeText()\{\
-  var element = document.getElementById("demo");\
-  element.innerHTML = "Text has been changed!";\
-\}\
-\
+  var element = document.getElementById("demo");
+  element.innerHTML = "Text has been changed!";
+}
+
 // Using setInterval to call the function every 3 seconds (3000 milliseconds)\
-setInterval(changeText, 3000);\
-\
+setInterval(changeText, 3000);
+
 // Use setTimeout to delay execution of a function\
-setTimeout(function()\{\
-  var element = document.getElementById("demo2");\
+setTimeout(function(){
+  var element = document.getElementById("demo2");
   element.innerHTML = "This text appeared after a 5 second delay!";\
-\}, 5000);\
-\
+}, 5000);
+
 // Function to handle onclick event\
-function handleClick()\{\
+function handleClick()\{
   alert('You clicked me!');\
 \}\
 \
